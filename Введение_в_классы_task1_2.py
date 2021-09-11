c2ad229e2c29cf8e3b16784e02bb64dce7b47c13
altaypower <altaypower@gmail.com>
2021-09-11 11:08:28 +0700

    Введение_в_классы_task1_2

---
+++ Введение_в_классы_task1_2.py
@@ -0,0 +1,32 @@
+"""
+1.
+В какой момент начинается исполнение конструктора класса?
+ответ:
+При создании каждого экземпляра класса
+
+2.
+Какие числа будут выведены в результате выполнения данного кода?
+class A:
+    def __init__(self, val=0):
+        self.val = val
+
+    def add(self, x):
+        self.val += x
+
+    def print_val(self):
+        print(self.val)
+
+
+a = A()
+b = A(2)
+c = A(4)
+a.add(2)
+b.add(2)
+
+a.print_val()
+b.print_val()
+c.print_val()
+
+ответ
+2, 4, 4
+"""
